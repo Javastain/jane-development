8ea0b30ea1bf27352b8623a64d81ee06fe8bc94f
Digicat509 <alexander.snyder@tagmagnet.org>
2026-06-13 19:37:44 -0500

    Interaction/Motion fix

--- Characters/player.gd
+++ Characters/player.gd
@@ -1,18 +1,40 @@
 extends CharacterBody2D
 
-@export var move_speed : float = 150
+@export var move_speed : float = 250
+
+
+var last_input_direction = Vector2.ZERO
+var input_direction = Vector2.ZERO
 
 func _physics_process(_delta):
 	# Get input direction
-	var input_direction = Vector2(
+	input_direction = Vector2(
 		Input.get_action_strength('right') - Input.get_action_strength('left'),
 		Input.get_action_strength('down') - Input.get_action_strength('up')
 	)
 	
-	print(input_direction)
+	input_direction = input_direction.normalized()
+	
+	if(Input.is_action_pressed("down") or
+	Input.is_action_pressed("up") or
+	Input.is_action_pressed("left") or 
+	Input.is_action_pressed("right")):
+		last_input_direction = input_direction
+	
+	if(Input.is_action_just_pressed("interact")):
+		#talk to npc if they are in front of the player
+		var space_state = get_world_2d().direct_space_state
+		var query = PhysicsRayQueryParameters2D.create(global_position, 
+		global_position + last_input_direction * 60)
+		var result = space_state.intersect_ray(query)
+		if(result.size() != 0 and result.collider != null and result.collider.is_in_group("npcs")):
+			talk();
 	
 	# Update velocity
 	velocity = input_direction * move_speed
 	
 	# Move
 	move_and_slide()
+
+func talk():
+	print("hi I'm Jane")
