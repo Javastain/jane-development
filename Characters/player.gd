6b8e53821529d74360c86456a4b1c9e512448e5f
Javastain <ivyjnedwards@gmail.com>
2026-06-14 03:38:48 -0700

    Dialogue improvements
    Also we have a gunshot sound now 👍

--- Characters/player.gd
+++ Characters/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+signal SELECTED(index)
+
 @export var move_speed : float = 250
 
 var textbox_scene = preload("res://UI/textbox.tscn")
@@ -26,10 +28,10 @@ func _physics_process(_delta):
 		#talk to npc if they are in front of the player
 		var space_state = get_world_2d().direct_space_state
 		var query = PhysicsRayQueryParameters2D.create(global_position, 
-		global_position + last_input_direction * 60)
+		global_position + (last_input_direction * 90))
 		var result = space_state.intersect_ray(query)
 		if(result.size() != 0 and result.collider != null and result.collider.is_in_group("npcs")):
-			talk();
+			talk(result.collider.name);
 	
 	# Update velocity
 	velocity = input_direction * move_speed
@@ -37,7 +39,29 @@ func _physics_process(_delta):
 	# Move
 	move_and_slide()
 
-func talk():
+func talk(character):
 	var textbox = textbox_scene.instantiate()
-	textbox.text_pages = ["Hi I'm Jane!"]
+	textbox.character = character
+	if(character == "Alex"):
+		textbox.text_pages = ["Alex dialogue."]
+	elif(character == "Asa"):
+		textbox.text_pages = ["Asa dialogue."]
+	elif(character == "David"):
+		textbox.text_pages = ["David dialogue."]
+	elif(character == "DebugJoe"):
+		textbox.text_pages = ["Yeah, I'm here too. I don't know.", "Just go with it, I guess.", "&...Okay!"]
+	elif(character == "Ivy"):
+		textbox.text_pages = ["Ivy dialogue."]
+	elif(character == "Jay"):
+		textbox.text_pages = ["Jay dialogue."]
+	elif(character == "Jeremie"):
+		textbox.text_pages = ["Jeremie dialogue."]
+	elif(character == "John"):
+		textbox.text_pages = ["John dialogue."]
+	elif(character == "Klaus"):
+		textbox.text_pages = ["Klaus dialogue."]
+	elif(character == "Ruby"):
+		textbox.text_pages = ["Ruby dialogue."]
+	else:
+		textbox.text_pages = ["Oops. This dialogue isn't ever supposed to show up. What happened?", "What did you do???"]
 	get_tree().get_root().add_child(textbox)
--- UI/textbox.gd
+++ UI/textbox.gd
@@ -3,6 +3,19 @@ extends Node2D
 var text_pages = []
 var initialized = false
 var page = 0
+var character = ""
+
+var alex_portrait = preload("res://Art/debug_joe_portrait.png")
+var asa_portrait = preload("res://Art/debug_joe_portrait.png")
+var david_portrait = preload("res://Art/debug_joe_portrait.png")
+var debug_joe_portrait = preload("res://Art/debug_joe_portrait.png")
+var ivy_portrait = preload("res://Art/debug_joe_portrait.png")
+var jane_portrait = preload("res://Art/jane_portrait.png")
+var jay_portrait = preload("res://Art/debug_joe_portrait.png")
+var jeremie_portrait = preload("res://Art/debug_joe_portrait.png")
+var john_portrait = preload("res://Art/debug_joe_portrait.png")
+var klaus_portrait = preload("res://Art/debug_joe_portrait.png")
+var ruby_portrait = preload("res://Art/debug_joe_portrait.png")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,7 +27,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if not initialized:
-		$Label.text = text_pages[page]
+		if (text_pages[page][0] == '&'):
+			$Speech.text = text_pages[page].erase(0, 1)
+		else:
+			$Speech.text = text_pages[page]
+		_update_identifiers()
 		initialized = true
 
 
@@ -24,4 +41,44 @@ func _physics_process(delta):
 		if page >= text_pages.size():
 			queue_free()
 		else:
-			$Label.text = text_pages[page]
+			if (text_pages[page][0] == '&'):
+				$Speech.text = text_pages[page].erase(0, 1)
+			else:
+				$Speech.text = text_pages[page]
+			_update_identifiers()
+
+func _update_identifiers():
+	if (text_pages[page][0] == '&'):
+		$Portrait.texture = jane_portrait
+		$Name.text = "Jane"
+	else:
+		if character == "Alex":
+			$Portrait.texture = alex_portrait
+			$Name.text = "Alex"
+		elif character == "Asa":
+			$Portrait.texture = asa_portrait
+			$Name.text = "Asa"
+		elif character == "David":
+			$Portrait.texture = david_portrait
+			$Name.text = "David"
+		elif character == "DebugJoe":
+			$Portrait.texture = debug_joe_portrait
+			$Name.text = "Debug Joe"
+		elif character == "Ivy":
+			$Portrait.texture = ivy_portrait
+			$Name.text = "Ivy"
+		elif character == "Jay":
+			$Portrait.texture = jay_portrait
+			$Name.text = "Jay"
+		elif character == "Jeremie":
+			$Portrait.texture = jeremie_portrait
+			$Name.text = "Jeremie"
+		elif character == "John":
+			$Portrait.texture = john_portrait
+			$Name.text = "John"
+		elif character == "Klaus":
+			$Portrait.texture = klaus_portrait
+			$Name.text = "Klaus"
+		elif character == "Ruby":
+			$Portrait.texture = ruby_portrait
+			$Name.text = "Ruby"
